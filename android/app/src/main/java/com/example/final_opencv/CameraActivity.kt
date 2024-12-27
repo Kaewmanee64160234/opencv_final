0e5d6476003383957de060c183a3fa60862e47d6
Kaewmanee <121871342+Kaewmanee64160234@users.noreply.github.com>
2024-12-27 11:38:14 +0700

    can auto capture

--- android/app/src/main/java/com/example/final_opencv/CameraActivity.kt
+++ android/app/src/main/java/com/example/final_opencv/CameraActivity.kt
@@ -2,8 +2,6 @@ package com.example.final_opencv
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -13,40 +11,29 @@ import androidx.annotation.RequiresApi
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInRoot
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import org.opencv.core.*
-import org.opencv.imgproc.Imgproc
+import java.io.File
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class CameraActivity : ComponentActivity() {
 
     private lateinit var cameraExecutor: ExecutorService
+    private lateinit var imageCapture: ImageCapture
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Load OpenCV native libraries
-        if (!org.opencv.android.OpenCVLoader.initDebug()) {
-            Log.e("OpenCV", "OpenCV initialization failed")
-        } else {
-            Log.d("OpenCV", "OpenCV initialization successful")
-        }
 
         // Request Camera Permissions
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -56,7 +43,7 @@ class CameraActivity : ComponentActivity() {
         cameraExecutor = Executors.newSingleThreadExecutor()
 
         setContent {
-            CameraPreviewWithRealTimeAnalysis()
+            CameraPreviewScreen()
         }
     }
 
@@ -66,23 +53,9 @@ class CameraActivity : ComponentActivity() {
     }
 
     @Composable
-    fun CameraPreviewWithRealTimeAnalysis() {
+    fun CameraPreviewScreen() {
         val context = this
         val previewView = remember { PreviewView(context) }
-
-        // Rectangle overlay properties
-        var rectX by remember { mutableStateOf(0) }
-        var rectY by remember { mutableStateOf(0) }
-        var rectWidth by remember { mutableStateOf(300) }
-        var rectHeight by remember { mutableStateOf(200) }
-
-        // Analysis values
-        var brightnessList = remember { mutableStateListOf<Double>() }
-        var glareList = remember { mutableStateListOf<Double>() }
-        var statusMessage by remember { mutableStateOf("Analyzing...") }
-
-        // Mutable state to store the last update timestamp
-        var lastUpdateTime by remember { mutableStateOf(System.currentTimeMillis()) }
 
         LaunchedEffect(Unit) {
             val cameraProvider = ProcessCameraProvider.getInstance(context).get()
@@ -91,56 +64,11 @@ class CameraActivity : ComponentActivity() {
                 .build()
                 .also { it.setSurfaceProvider(previewView.surfaceProvider) }
 
-            val imageAnalysis = ImageAnalysis.Builder()
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            imageCapture = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
                 .build()
 
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            imageAnalysis.setAnalyzer(cameraExecutor) { imageProxy ->
-                val croppedBitmap = cropImageProxyToRect(
-                    imageProxy,
-                    rectX,
-                    rectY,
-                    rectWidth,
-                    rectHeight,
-                    previewView.width,
-                    previewView.height
-                )
-                if (croppedBitmap != null) {
-                    val mat = bitmapToMat(croppedBitmap)
-                    if (mat != null && !mat.empty()) {
-                        val brightness = calculateBrightness(mat)
-                        val brightRegionStatus = analyzeBrightRegions(mat)
-
-                        brightnessList.add(brightness)
-
-                        if (System.currentTimeMillis() - lastUpdateTime >= 1000) {
-                            // Calculate averages
-                            val avgBrightness = brightnessList.average()
-
-                            // Update status message
-                            statusMessage = when {
-                                avgBrightness < 81 -> "Brightness too low. Increase lighting."
-                                avgBrightness > 155 -> "Brightness too high. Reduce lighting."
-                                brightRegionStatus == "Watermark Detected" -> "Watermark detected."
-                                brightRegionStatus == "Reflection Detected" -> "Reflection detected."
-                                else -> "Lighting conditions are optimal."
-                            }
-
-                            // Clear lists for next interval
-                            brightnessList.clear()
-
-                            // Update the last update time
-                            lastUpdateTime = System.currentTimeMillis()
-                        }
-
-                        mat.release()
-                    }
-                }
-                imageProxy.close()
-            }
-
 
             try {
                 cameraProvider.unbindAll()
@@ -148,7 +76,7 @@ class CameraActivity : ComponentActivity() {
                     context,
                     cameraSelector,
                     preview,
-                    imageAnalysis
+                    imageCapture
                 )
             } catch (exc: Exception) {
                 Log.e("CameraActivity", "Failed to bind use cases", exc)
@@ -161,201 +89,61 @@ class CameraActivity : ComponentActivity() {
                 factory = { previewView }
             )
 
-            RectangleOverlay(
-                modifier = Modifier.align(Alignment.Center),
-                onOverlayPositioned = { x, y, width, height ->
-                    rectX = x
-                    rectY = y
-                    rectWidth = width
-                    rectHeight = height
-                }
-            )
-
             Column(
                 modifier = Modifier
-                    .align(Alignment.TopCenter)
+                    .align(Alignment.BottomCenter)
                     .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(statusMessage, style = TextStyle(fontSize = 18.sp, color = Color.White))
-            }
-        }
-    }
-
-
-    private fun analyzeBrightRegions(mat: Mat): String {
-        val gray = Mat()
-        Imgproc.cvtColor(mat, gray, Imgproc.COLOR_BGR2GRAY)
-
-        val binary = Mat()
-        Imgproc.threshold(gray, binary, 230.0, 255.0, Imgproc.THRESH_BINARY)
-
-        val contours = ArrayList<MatOfPoint>()
-        val hierarchy = Mat()
-        Imgproc.findContours(binary, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE)
-
-        for (contour in contours) {
-            val area = Imgproc.contourArea(contour)
-            if (area > 100) { // Ignore small noise
-                val perimeter = Imgproc.arcLength(MatOfPoint2f(*contour.toArray()), true)
-                val circularity = 4 * Math.PI * (area / (perimeter * perimeter))
-
-                if (circularity > 0.7) {
-                    gray.release()
-                    binary.release()
-                    return "Watermark Detected"
-                } else {
-                    gray.release()
-                    binary.release()
-                    return "Reflection Detected"
+                Button(
+                    onClick = {
+                        captureBurstImages(imageCapture, 5)
+                    }
+                ) {
+                    Text("Capture 5 Images")
                 }
             }
         }
-
-        gray.release()
-        binary.release()
-        return "No significant regions detected"
     }
-    private fun cropImageProxyToRect(
-        imageProxy: ImageProxy,
-        rectX: Int,
-        rectY: Int,
-        rectWidth: Int,
-        rectHeight: Int,
-        previewWidth: Int,
-        previewHeight: Int
-    ): Bitmap? {
-        val bitmap = imageProxyToBitmap(imageProxy) ?: return null
 
-        // Map the rectangle dimensions to the bitmap size
-        val scaleX = bitmap.width.toFloat() / previewWidth
-        val scaleY = bitmap.height.toFloat() / previewHeight
+    private fun captureBurstImages(imageCapture: ImageCapture, totalCaptures: Int = 5) {
+        var currentCapture = 0
+        val photoFiles = mutableListOf<File>()
 
-        val cropX = (rectX * scaleX).toInt()
-        val cropY = (rectY * scaleY).toInt()
-        val cropWidth = (rectWidth * scaleX).toInt()
-        val cropHeight = (rectHeight * scaleY).toInt()
-
-        if (cropWidth <= 0 || cropHeight <= 0 || cropX < 0 || cropY < 0 ||
-            cropX + cropWidth > bitmap.width || cropY + cropHeight > bitmap.height
-        ) {
-            Log.e("CropError", "Invalid crop dimensions: x=$cropX, y=$cropY, width=$cropWidth, height=$cropHeight")
-            return null
+        for (i in 0 until totalCaptures) {
+            val photoFile = File(
+                externalMediaDirs.firstOrNull(),
+                "IMG_${System.currentTimeMillis()}_$i.jpg"
+            )
+            photoFiles.add(photoFile)
         }
 
-        return Bitmap.createBitmap(bitmap, cropX, cropY, cropWidth, cropHeight)
-    }
-
-    private fun imageProxyToBitmap(imageProxy: ImageProxy): Bitmap? {
-        return try {
-            val plane = imageProxy.planes[0]
-            val buffer = plane.buffer
-            val bytes = ByteArray(buffer.remaining())
-            buffer.get(bytes)
-
-            val yuvImage = android.graphics.YuvImage(
-                bytes,
-                android.graphics.ImageFormat.NV21,
-                imageProxy.width,
-                imageProxy.height,
-                null
-            )
-
-            if (imageProxy.width <= 0 || imageProxy.height <= 0) {
-                Log.e("ImageProxyError", "Invalid image dimensions: width=${imageProxy.width}, height=${imageProxy.height}")
-                return null
+        fun captureNextImage() {
+            if (currentCapture >= totalCaptures) {
+                Log.d("Burst", "Captured $totalCaptures images.")
+                return
             }
 
-            val outputStream = java.io.ByteArrayOutputStream()
-            yuvImage.compressToJpeg(
-                android.graphics.Rect(0, 0, imageProxy.width, imageProxy.height),
-                100,
-                outputStream
-            )
-            val jpegBytes = outputStream.toByteArray()
-            BitmapFactory.decodeByteArray(jpegBytes, 0, jpegBytes.size)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
-    }
+            val photoFile = photoFiles[currentCapture]
+            val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
-    private fun bitmapToMat(bitmap: Bitmap): Mat? {
-        return try {
-            val mat = Mat()
-            org.opencv.android.Utils.bitmapToMat(bitmap, mat)
-            mat
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
-    }
+            imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(this),
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                        Log.d("Burst", "Image ${currentCapture + 1} saved: ${photoFile.absolutePath}")
+                        currentCapture++
+                        captureNextImage()
+                    }
 
-    private fun calculateBrightness(mat: Mat): Double {
-        return try {
-            val gray = Mat()
-            Imgproc.cvtColor(mat, gray, Imgproc.COLOR_BGR2GRAY)
-            val meanIntensity = Core.mean(gray).`val`[0]
-            gray.release()
-            meanIntensity
-        } catch (e: Exception) {
-            e.printStackTrace()
-            -1.0
-        }
-    }
-
-//    private fun calculateGlarePercentage(mat: Mat): Double {
-//        return try {
-//            val gray = Mat()
-//            Imgproc.cvtColor(mat, gray, Imgproc.COLOR_BGR2GRAY)
-//
-//            val binary = Mat()
-//            Imgproc.threshold(gray, binary, 230.0, 255.0, Imgproc.THRESH_BINARY)
-//
-//            val glarePixels = Core.countNonZero(binary)
-//            val totalPixels = mat.rows() * mat.cols()
-//
-//            gray.release()
-//            binary.release()
-//
-//            if (totalPixels > 0) {
-//                (glarePixels.toDouble() / totalPixels.toDouble()) * 100.0
-//            } else {
-//                0.0
-//            }
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            0.0
-//        }
-//    }
-}
-
-@Composable
-fun RectangleOverlay(
-    modifier: Modifier = Modifier,
-    onOverlayPositioned: (Int, Int, Int, Int) -> Unit
-) {
-    Box(
-        modifier = modifier
-            .size(300.dp, 200.dp) // Fixed size for rectangle
-            .border(2.dp, Color.Red, RoundedCornerShape(13.dp))
-            .onGloballyPositioned { coordinates ->
-                val position = coordinates.positionInRoot()
-                val width = coordinates.size.width
-                val height = coordinates.size.height
-
-                Log.d("RectangleOverlay", "Rectangle dimensions: x=${position.x}, y=${position.y}, width=$width, height=$height")
-
-                if (width > 0 && height > 0) {
-                    onOverlayPositioned(
-                        position.x.toInt(),
-                        position.y.toInt(),
-                        width,
-                        height
-                    )
-                } else {
-                    Log.e("OverlayError", "Invalid rectangle dimensions: width=$width, height=$height")
+                    override fun onError(exception: ImageCaptureException) {
+                        Log.e("Burst", "Error capturing image ${currentCapture + 1}: ${exception.message}")
+                    }
                 }
-            }
-    )
+            )
+        }
+
+        captureNextImage()
+    }
 }
